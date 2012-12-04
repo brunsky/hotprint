571689482b340247dd11cbc0ea3ee3073fb2cbdb
brunsky <will.lien@gmail.com>
2012-12-04 10:04:47 +0800

    1. 加入購物車模組(待測) 2. 獨立 save、random 為擴充模組 3. 準備整理可供上線運轉的頁面

---
+++ js/extention.js
@@ -0,0 +1,213 @@
+//////////////////////////////////////////////////
+// Sane design into image source
+
+var scaling = 3; // Image saving ratio
+ 
+function mod_saving() {
+	$("body").append('<div class="modalOverlay"></div>');
+	$wDiv = $('.modalOverlay');
+	$("body").append('<div id="progress-bar"><div id="status"></div></div>');
+	$( "#progress-bar" ).css('position','fixed');
+	$( "#progress-bar" ).css('z-index', '1001');
+	$( "#progress-bar" ).css('top', '300px');
+	$( "#progress-bar" ).css('left', '500px');
+	$status = $('#status');
+	var a = new Image();
+	a.src = "images/"+PHONE_NAME+"_mask2.png";
+	a.onload = function(){
+	
+		var resCanvas = document.createElement('canvas');
+	    var resCtx = resCanvas.getContext('2d');
+		resCanvas.width = $("#mCanvas")[0].width * scaling;
+		resCanvas.height = $("#mCanvas")[0].height * scaling;
+		
+		// scaling mask
+		var adjustMaskCanvas = document.createElement('canvas');
+	    adjustMaskCanvas.width = a.width;
+	    adjustMaskCanvas.height = a.height;
+	    var adjustMaskCtx = adjustMaskCanvas.getContext('2d');                       
+	    adjustMaskCtx.drawImage(a, 0, 0); 
+		
+		var factor = 100 / $('.layout_corner').length; 
+		var inc = 0;
+		
+		var elements = $('body').find('.layout_corner');
+		var index = 0;
+		process();
+		
+		// Using settimeout to let browser update screen.
+		function process() {
+			var e = elements.get(index++);
+			if ($(e).length) {
+
+				$status.css('width', function(){
+					inc += factor;
+					return inc+"%";
+				});
+	
+				
+		    	if (typeof $(e).next()[0] != 'undefined' &&
+		    		$(e).next()[0].tagName.toLowerCase() != 'canvas'.toLowerCase()) {
+					return true; // the same as continue in C :)
+				}
+		    	
+				var $c = $('<div></div>');
+				
+				$c.css('position', 'absolute');
+				$c.css('top', parseInt($(e).css('top'), 10) * scaling + 'px');
+				$c.css('left', parseInt($(e).css('left'), 10) * scaling + 'px');
+				$c.css('width', parseInt($(e).css('width'), 10) * scaling + 'px');
+				$c.css('height', parseInt($(e).css('height'), 10) * scaling + 'px');
+				$c.css('border', '6px solid silver'); // setup border in manually !!
+				//$c.css('border', $(this).css('border'));
+				//$c.css('border-left-width', parseInt($(this).css('border-left-width'), 10) * scaling + 'px');
+				$('body').append($c);
+				
+				var $imgSrc = $(e).children(".draggable");
+				//$imgSrc.css('width', $imgSrc[0].width+'px');
+				//$imgSrc.css('height',$imgSrc[0].height+'px');
+				$imgSrc.css('width', '612px');
+				$imgSrc.css('height','612px');
+		
+		    	if ($(e).attr("class").indexOf("layout_circle") >= 0) {
+					doClipping( 
+						doMasking($imgSrc[0], adjustMaskCanvas, $c, layout_x*scaling, layout_y*scaling), 
+						$c, 
+						cirClipper);	
+				}
+				else if ($(e).attr("class").indexOf("layout_square") >= 0) {
+					doClipping( 
+						doMasking($imgSrc[0], adjustMaskCanvas, $c, layout_x*scaling, layout_y*scaling), 
+						$c, 
+						boxClipper);
+				}
+			
+				resCtx.drawImage($c.next()[0], 
+		    					(parseInt($c.next().css('left'), 10)-layout_x*scaling), 
+		    					(parseInt($c.next().css('top'), 10)-layout_y*scaling), 
+		    					parseInt($c.next().css('width'), 10),
+		    					parseInt($c.next().css('height'), 10));
+		    				
+		    	$c.next().remove();
+		    	$c.remove();
+		    	$imgSrc.css('width', '');
+				$imgSrc.css('height','');
+				
+				setTimeout(process, 10);
+			}
+			else {
+				var img = $('<img>');
+				img.attr('id', 'canvasImg');
+				img.css('position', 'absolute');
+				img.css('top', '80px');
+				img.css('left', '800px');
+				img.css('width', $("#mCanvas")[0].width*0.7+'px');
+				img.css('height', $("#mCanvas")[0].height*0.7+'px');
+				$('body').append(img);
+				$('#canvasImg')[0].src = resCanvas.toDataURL();
+					
+				$( "#progress-bar" ).remove();
+				$wDiv.remove();
+			}
+		}
+	}
+}
+
+/*
+ * Generize random design
+ */
+function mod_randesign() {
+	// clear content
+	$('.layout_corner').css('opacity', CORNER_OPT);
+	$('.layout_corner').html('');
+	$('.canvas_appended').remove();
+	// visit image gallery in randomly
+	$('.layout_corner').each(function(index) {
+
+		var r = Math.floor(Math.random() * $('.gallery-pool').length);
+		// put image into layout
+		$(this).append($($('.gallery-pool')[r]).clone());
+		$(this).children(".draggable").removeClass('gallery-pool'); // remove to prevent getting the same one
+		$(this).children(".draggable").css({
+			"position": "relative",
+			"top": "0px",
+			"left": "0px",
+			"width": $(this).css('width'),
+			"height": $(this).css('height'),
+			"opacity": "0"
+		});
+		$(this).children(".draggable").draggable({
+			cursor: 'auto', 
+			distance: 10, 
+			cursorAt: { left: 50, top: 50 },
+			revert:"invalid",
+			drag: function(event, ui) {
+				if ($(this).attr("class").indexOf("ui-draggable-dragging") >= 0) {
+					$(this).parent().css('z-index','1000');
+					$(this).css("opacity", "0.7");
+				}
+			},
+			start: function(event, ui) {
+				$(this).height(100).width(100);
+				$('.layout_corner').css('z-index', '998'); 
+				$('.layout_corner').animate({ opacity: 1 });
+				// set corner object
+				$(this).data('corner', $(this).parent());
+			}
+		});
+		
+		var imgSrc = $(this).children(".draggable").attr('src');
+		var divObj = $(this);
+		
+		var opts = {
+		  lines: 13, // The number of lines to draw
+		  length: 7, // The length of each line
+		  width: 4, // The line thickness
+		  radius: 10, // The radius of the inner circle
+		  corners: 1, // Corner roundness (0..1)
+		  rotate: 0, // The rotation offset
+		  color: '#000', // #rgb or #rrggbb
+		  speed: 1, // Rounds per second
+		  trail: 60, // Afterglow percentage
+		  shadow: false, // Whether to render a shadow
+		  hwaccel: false, // Whether to use hardware acceleration
+		  className: 'spinner', // The CSS class to assign to the spinner
+		  zIndex: 2e9, // The z-index (defaults to 2000000000)
+		  top: 'auto', // Top position relative to parent in px
+		  left: 'auto' // Left position relative to parent in px
+		};
+
+		var spinner = new Spinner(opts).spin($(this)[0]);
+		$.getImageData({
+			url: imgSrc,
+			server: "http://insta.camangiwebstation.com/proxy/getImageData.php",
+			success: function(image){
+				divObj.children(".draggable").attr('src', image.src);
+				divObj.children(".draggable").addClass("cached");
+				if (divObj.attr("class").indexOf("layout_circle") >= 0) {
+					doClipping( 
+						doMasking(image, maskImg, divObj, layout_ox, layout_oy), 
+						divObj, 
+						cirClipper);	
+				}
+				else if (divObj.attr("class").indexOf("layout_square") >= 0) {
+					doClipping( 
+						doMasking(image, maskImg, divObj, layout_ox, layout_oy), 
+						divObj, 
+						boxClipper);
+				}
+				// set corner div to invisible
+				divObj.css('opacity','0');
+				spinner.stop();
+			},
+			error: function(xhr, text_status){
+				spinner.stop();
+				console.log("Fail to get image:"+imgSrc);
+			}
+		});
+		
+		$(this).children(".draggable").css('z-index','');		
+		$(this).css('cursor', 'move');
+		$(this).css('z-index','998');
+	});
+}
