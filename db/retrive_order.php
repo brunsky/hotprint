f7f347789586ff85336463c8fbdd2ebe76203ea5
brunsky <will.lien@gmail.com>
2013-03-15 17:56:15 +0800

    加入取回訂單的 php

---
+++ db/retrive_order.php
@@ -0,0 +1,25 @@
+<?php
+include "db.inc";
+include "setup.inc";
+
+$db = new DB;
+$connkey = $db->link_sip( "localhost", $DB_NAME, "root", "tomorrow");
+if ($connkey) {
+
+	$sql = sprintf(
+        "SELECT * from order_list WHERE userid='%s'", 
+        	mysql_real_escape_string($_POST["userid"])
+		);
+	
+	$result = $db->sql($sql);	
+	$data = array();
+	for ($i=0; $i<count($result); $i++) {
+		$a = array( 'order_no'=> $result[$i]['order_no'],
+					'c_time_local'=> $result[$i]['c_time_local'],
+					'payment'=> $result[$i]['payment'],
+					'detail'=> $result[$i]['detail']);
+		array_push($data, $a);
+	}
+	echo json_encode($data);
+}
+?>
